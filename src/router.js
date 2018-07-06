e0a75cff9b93d8f2c626bfa0645893081556c179
Dr. Pit Lazarus <drpitlazarus@gmail.com>
2018-07-06 14:30:40 -0400

    Add vue-meta

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueMeta from 'vue-meta'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Player from './views/Player.vue'
+import PlayerSummary from './views/Player/PlayerSummary'
 
 Vue.use(Router)
+Vue.use(VueMeta)
 
 export default new Router({
   routes: [
@@ -21,7 +24,14 @@ export default new Router({
     {
       path: '/player',
       name: 'player',
-      component: Player
+      component: Player,
+      children: [
+        {
+          path: ':steamId64(\\d{17})/:playerName?',
+          name: 'player-summary',
+          component: PlayerSummary
+        }
+      ]
     }
   ]
 })
